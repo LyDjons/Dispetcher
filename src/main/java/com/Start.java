9c1282369e1a92a1f5f015c87d896a578d149134
LyDjons <znahar917@gmail.com>
2014-12-08 23:29:41 +0200

    create test maven

---
+++ src/main/java/com/Start.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.apache.poi.ss.usermodel.Sheet;
+
+/**
+ * Created by LyDjons on 08.12.2014.
+ */
+public class Start {
+    public static void main(String []args){
+        System.out.println("Hello world");
+        Sheet sheet;
+    }
+}
